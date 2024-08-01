e882acca6778f5ed4b0405e41423f2eabd9dcc38
Mohammad Javad Najafirad <mjnajafi@yahoo.com>
2024-08-01 12:40:48 -0400

    Update GenerateDG.m

--- GenerateDG.m
+++ GenerateDG.m
@@ -7,18 +7,18 @@ function DG = GenerateDG(R0,L0,C0,RL0)
     RLi = RL0 + 0.01*rand();
     Yi = 1 / RLi;
     
-    A_i = [-Yi/Ci, 1/Ci, 0;
+    Ai = [-Yi/Ci, 1/Ci, 0;
                -1/Li, -Ri/Li, 0;
                 1, 0, 0];
     
-    B_i = [0; 1/Li; 0];
+    Bi = [0; 1/Li; 0];
     
     % Storing
     DG.R = Ri;
     DG.L = Li;
     DG.C = Ci;
     DG.Y = Yi;
-    DG.A = A_i;
-    DG.B = B_i;
+    DG.A = Ai;
+    DG.B = Bi;
 end
 
