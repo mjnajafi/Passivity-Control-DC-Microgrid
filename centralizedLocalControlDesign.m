a9cfc510df9e89aa5dc962066f9330125ea6aca4
Mohammad Javad Najafirad <mjnajafi@yahoo.com>
2024-08-01 16:39:07 -0400

    Update centralizedLocalControlDesign.m

--- centralizedLocalControlDesign.m
+++ centralizedLocalControlDesign.m
@@ -21,7 +21,39 @@ constraints = [];
 %% Combine constraints over all DGs (66a-Part1, 66b,66d,66e)
 for i = 1:1:numOfDGs
 
-    constraints = [constraints, newConstraints]
+    % Constraint (66a-Part1)
+    con1 = P_i{i} >= 0;
+    
+    % Constraint (66b)
+    DMat = rhoTilde_i{i} * eye(3);
+    MMat = [P_i{i}, zeros(3)];
+    ThetaMat = [-DG.A{i}*P_i{i} - P_i{i}'*DG.A{i}' - DG.B{i}*K_i{i} - K_i{i}'*DG.B{i}', -eye(3) + 0.5*P_i{i};
+                -eye(3) + 0.5*P_i{i}, -nu_i{i}*eye(3)];
+    W = [DMat, MMat; MMat', ThetaMat];
+    
+    con2 = W >= 0;
+    
+    % Constraint (66d)
+    p_i = 1;    % predefined value
+    
+    con3_1 = -gammaTilde_i{i}/p_i <= nu_i{i};
+    con3_2 = nu_i{i} <= 0;
+    
+
+    % Constraint (66e)
+    con4_1 = 0 <= rhoTilde_i{i};
+
+    con4_21 = rhoTilde_i{i} <= p_i;
+    con4_22 = rhoTilde_i{i} <= 4*gammaTilde_i{i}/p_i;
+    
+    % Collecting Constraints
+    constraints = [constraints, con1];
+    constraints = [constraints, con2];
+    constraints = [constraints, con3_1];
+    constraints = [constraints, con3_2];
+    constraints = [constraints, con4_1];
+    constraints = [constraints, con4_21];
+    constraints = [constraints, con4_22];
 end
 
 
