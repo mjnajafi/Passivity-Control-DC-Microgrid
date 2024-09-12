45b5b2d0c7bb57d3affd5fec77985a0fd7efcf19
Mohammad Javad Najafirad <mjnajafi@yahoo.com>
2024-09-12 18:37:52 -0400

    more variations on parameters

--- GenerateDG.m
+++ GenerateDG.m
@@ -1,35 +1,36 @@
 % This function, GenerateDG, generates a Distributed Generator (DG) with
-% randomly perturbed electrical parameters based on initial values provided 
-% as inputs (R0, L0, C0, RL0). The function creates and stores the system's 
-% attributes, including the resistance (R), inductance (L), capacitance (C), 
-% load admittance (Y), state-space matrices (A and B), and coordinates 
-% (coords_i) in a structure named DG.
+% more significant variations in electrical parameters based on initial values 
+% provided as inputs (R0, L0, C0, RL0). The function creates and stores the 
+% system's attributes, including resistance (R), inductance (L), capacitance (C), 
+% load admittance (Y), state-space matrices (A and B), and coordinates (coords_i) 
+% in a structure named DG.
 
-function DG = GenerateDG(R0,L0,C0,RL0,IL0,coords_i)
+function DG = GenerateDG(R0, L0, C0, RL0, IL0, coords_i)
 
-    % Generating
-    Ri = R0 + 0.01*rand();
-    Li = L0 + 0.01*rand();
-    Ci = C0 + 0.001*rand();
-    RLi = RL0 + 0.01*rand();
-    Yi = 1 / RLi;
-    ILi = IL0 + 0.1*rand();
+    % Introduce more significant variations in DG parameters
+    % For example, vary parameters within a larger range
+    variationFactorR = 0.5;   % 50% variation in R
+    variationFactorL = 0.2;   % 20% variation in L
+    variationFactorC = 0.3;   % 30% variation in C
+    variationFactorRL = 0.4;  % 40% variation in RL
+    variationFactorIL = 0.3;  % 30% variation in IL
 
-   
+    % Generate new values for the parameters with larger variations
+    Ri = R0 * (1 + variationFactorR * (2*rand() - 1));   
+    Li = L0 * (1 + variationFactorL * (2*rand() - 1));   
+    Ci = C0 * (1 + variationFactorC * (2*rand() - 1));   
+    RLi = RL0 * (1 + variationFactorRL * (2*rand() - 1)); 
+    Yi = 1 / RLi;    
+    ILi = IL0 * (1 + variationFactorIL * (2*rand() - 1)); 
 
-    
+    % State-space matrices A and B
     Ai = [-Yi/Ci, 1/Ci, 0;
-               -1/Li, -Ri/Li, 0;
-                1, 0, 0];
+          -1/Li, -Ri/Li, 0;
+           1, 0, 0];
 
-
-   
-    
-    
     Bi = [0; 1/Li; 0];
 
-       
-    % Storing
+    % Storing the generated values in the DG structure
     DG.R = Ri;
     DG.L = Li;
     DG.C = Ci;
@@ -40,3 +41,40 @@ function DG = GenerateDG(R0,L0,C0,RL0,IL0,coords_i)
     DG.coordinates = coords_i;
 end
 
+
+% 
+% function DG = GenerateDG(R0,L0,C0,RL0,IL0,coords_i)
+% 
+%     % Generating
+%     Ri = R0 + 0.01*rand();
+%     Li = L0 + 0.01*rand();
+%     Ci = C0 + 0.001*rand();
+%     RLi = RL0 + 0.01*rand();
+%     Yi = 1 / RLi;
+%     ILi = IL0 + 0.1*rand();
+% 
+% 
+% 
+% 
+%     Ai = [-Yi/Ci, 1/Ci, 0;
+%                -1/Li, -Ri/Li, 0;
+%                 1, 0, 0];
+% 
+% 
+% 
+% 
+% 
+%     Bi = [0; 1/Li; 0];
+% 
+% 
+%     % Storing
+%     DG.R = Ri;
+%     DG.L = Li;
+%     DG.C = Ci;
+%     DG.Y = Yi;
+%     DG.IL = ILi;
+%     DG.A = Ai;
+%     DG.B = Bi;
+%     DG.coordinates = coords_i;
+% end
+% 
--- GenerateLine.m
+++ GenerateLine.m
@@ -1,22 +1,45 @@
-% This function, GenerateLine, generates a transmission line with 
-% randomly perturbed electrical parameters based on initial values provided 
-% as inputs (Rl0, Ll0). The function creates and stores the line's attributes, 
-% including the resistance (R), inductance (L), and state-space matrices (A and B)
-% in a structure named Line.
+% This function, GenerateLine, generates a transmission line with
+% more significant variations in electrical parameters based on initial 
+% values provided as inputs (Rl0, Ll0). The function creates and stores the line's 
+% attributes, including resistance (R), inductance (L), and state-space 
+% matrices (A and B) in a structure named Line.
 
-function Line = GenerateLine(Rl0,Ll0)
+function Line = GenerateLine(Rl0, Ll0)
 
-    % Generating
-    Rl = Rl0 + 0.01*rand();
-    Ll = Ll0 + 0.01*rand();
+    % Introduce more significant variations in line parameters
+    % For example, vary parameters within a larger range
+    variationFactorRl = 0.4;  % 40% variation in line resistance
+    variationFactorLl = 0.3;  % 30% variation in line inductance
 
+    % Generate new values for the line parameters with larger variations
+    Rl = Rl0 * (1 + variationFactorRl * (2*rand() - 1));  
+    Ll = Ll0 * (1 + variationFactorLl * (2*rand() - 1));  
+
+    % State-space matrices A and B for the transmission line
     Al = -Rl / Ll;
     Bl = 1 / Ll;
 
-    % Storing
+    % Storing the generated values in the Line structure
     Line.R = Rl;
     Line.L = Ll;
     Line.A = Al;
     Line.B = Bl;
 end
+
+% 
+% function Line = GenerateLine(Rl0,Ll0)
+% 
+%     % Generating
+%     Rl = Rl0 + 0.01*rand();
+%     Ll = Ll0 + 0.01*rand();
+% 
+%     Al = -Rl / Ll;
+%     Bl = 1 / Ll;
+% 
+%     % Storing
+%     Line.R = Rl;
+%     Line.L = Ll;
+%     Line.A = Al;
+%     Line.B = Bl;
+% end
 
